e0d1587183b1cd54f870322642e26500ea833d12
Victor Moreira <victormoreira247@gmail.com>
2025-09-15 23:18:28 -0300

    Add files via upload

---
+++ shell.py
@@ -0,0 +1,53 @@
+import os
+
+# Função para verificar inteiros
+def isint(x):
+    try:
+        return int(x)
+    except ValueError:
+        return 'False'
+
+
+# Inicialização de lista para armazenar os comandos utilizados
+historico = []
+
+print('\nPara lista de comandos, insira o comando "help".\n')
+
+while True:
+    comando = input('>> Digite o comando:\n>> ')
+    print('\n>> Caso queira sair, digite "exit".\n')
+
+    # Mudança de diretório
+    if comando.startswith('cd '):
+        try:
+            if comando == 'cd ..':
+                os.chdir('..')
+            else:
+                os.chdir(comando[3:])
+        except FileNotFoundError:
+            print('>> Diretório não encontrado.')
+            continue
+
+    # Listagem de comandos pelo histórico
+    elif comando[0] == '!' and len(comando) > 1:
+        if comando == '!!' and len(historico) >= 1:
+            comando = historico[-1]
+        else:
+            index = isint(comando[1:])
+            if index == 'False':
+                print('>> Comando não suportado.')
+                continue
+            if index > len(historico):
+                print('>> Não há comandos suficiente.')
+                continue
+            comando = historico[int(comando[1:])]
+    
+    # Mecanismo de parada do laço infinito
+    elif comando.lower() == 'exit':
+        exit()
+
+    # Caso não seja nenhum dos casos especiais acima, executar o comando
+    else:
+        os.system(comando)
+
+    historico.append(comando)
