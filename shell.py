d021debca3c3158afe01462e27f32eee59a3fa0e
Victor Moreira <victormoreira247@gmail.com>
2025-09-17 10:38:47 -0300

    Update shell.py

--- shell.py
+++ shell.py
@@ -15,54 +15,56 @@ historico = []
 print('\nPara lista de comandos, insira o comando "help".')
 print('Para visualizar o histórico, insira "historico".\n')
 
-while True:
-    print('\n>> Caso queira sair, digite "exit".\n')
-    comando = input('>> Digite o comando:\n>> ')
-
-    # Mecanismo de parada do laço infinito
-    if comando.lower() == 'exit':
-        exit()
-
-    # Mudança de diretório
-    elif comando.startswith('cd '):
-        try:
-            if comando == 'cd ..':
-                os.chdir('..')
+def main():
+    while True:
+        print('\n>> Caso queira sair, digite "exit".\n')
+        comando = input('>> Digite o comando:\n>> ')
+    
+        # Mecanismo de parada do laço infinito
+        if comando.lower() == 'exit':
+            exit()
+    
+        # Mudança de diretório
+        elif comando.startswith('cd '):
+            try:
+                if comando == 'cd ..':
+                    os.chdir('..')
+                else:
+                    os.chdir(comando[3:])
+            except FileNotFoundError:
+                print('>> Diretório não encontrado.')
+                continue
+    
+        # Listagem de comandos pelo histórico
+        elif comando[0] == '!' and len(comando) > 1:
+            if comando == '!!' and len(historico) >= 1:
+                comando = historico[-1]
             else:
-                os.chdir(comando[3:])
-        except FileNotFoundError:
-            print('>> Diretório não encontrado.')
-            continue
-
-    # Listagem de comandos pelo histórico
-    elif comando[0] == '!' and len(comando) > 1:
-        if comando == '!!' and len(historico) >= 1:
-            comando = historico[-1]
+                index = isint(comando[1:])
+                if index == 'False':
+                    print('>> Comando não suportado.')
+                    continue
+                if index > len(historico):
+                    print('>> Não há comandos suficiente.')
+                    continue
+                comando = historico[int(comando[1:])]
+        
+        # Visualização do histórico
+        elif comando == 'historico':
+            if len(historico) == 0:
+                print('>> Ainda não há comandos no histórico.')
+            else:
+                print('>> Os comandos inseridos estão na seguinte lista:')
+                for i in range(len(historico)):
+                    print(f'[{i}] {historico[i]')
+        
+        # Caso não seja nenhum dos casos especiais acima, executar o comando
         else:
-            index = isint(comando[1:])
-            if index == 'False':
-                print('>> Comando não suportado.')
-                continue
-            if index > len(historico):
-                print('>> Não há comandos suficiente.')
-                continue
-            comando = historico[int(comando[1:])]
-    
-    # Visualização do histórico
-    elif comando == 'historico':
-        if len(historico) == 0:
-            print('>> Ainda não há comandos no histórico.')
-        else:
-            print('>> Os comandos inseridos estão na seguinte lista:')
-            for i in range(len(historico)):
-                print(f'[{i}] {historico[i]')
-    
-    # Caso não seja nenhum dos casos especiais acima, executar o comando
-    else:
-        os.system(comando)
-    
-    # Armazena o comando no histórico
-    historico.append(comando)
+            os.system(comando)
+        
+        # Armazena o comando no histórico
+        historico.append(comando)
+main()
 
 
 
